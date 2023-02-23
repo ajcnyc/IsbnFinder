eacdc7d1e0a1741de0a717146023cae572372db1
Alex Cohen <alexanderjosephcohen@gmail.com>
2023-02-23 17:45:19 -0500

    Coded & Commented ApacheIsbn10Validator to the existing simple tests

--- isbnfinder/src/main/java/com/eplan/isbnfinder/validate/apache/validators/ApacheIsbn10Validator.java
+++ isbnfinder/src/main/java/com/eplan/isbnfinder/validate/apache/validators/ApacheIsbn10Validator.java
@@ -1,5 +1,7 @@
 package com.eplan.isbnfinder.validate.apache.validators;
 
+import org.apache.commons.validator.routines.ISBNValidator;
+
 import com.eplan.isbnfinder.validate.Isbn;
 import com.eplan.isbnfinder.validate.apache.ApacheIsbnValidator;
 
@@ -11,9 +13,27 @@ import com.eplan.isbnfinder.validate.apache.ApacheIsbnValidator;
  */
 public class ApacheIsbn10Validator implements ApacheIsbnValidator {
 
+	private ISBNValidator apacheValidator_; // The Apache ISBNValidator to use
+	
+	/**
+	 * Creates an ApacheIsbn10Validator
+	 */
+	public ApacheIsbn10Validator() {
+		apacheValidator_ = new ISBNValidator();
+	}
+
+	
 	@Override
 	public Isbn[] validate(String... isbns) {
-		return null;
+		// Get validity
+		boolean validity = apacheValidator_.isValidISBN10(isbns[0]);
+		
+		// Construct Isbn Object
+		Isbn isbn = new Isbn(isbns[0],validity);
+		
+		// Put it in an array and return
+		Isbn[] ret = {isbn};
+		return ret;
 	}
  
 }
