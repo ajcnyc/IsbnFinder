1db8583f063cbd2f31ba7dd3f8c9d061918d59a2
Alex Cohen <alexanderjosephcohen@gmail.com>
2023-02-23 01:28:13 -0500

    Created blank tests

---
+++ isbnfinder/src/test/java/com/eplan/isbnfinder/validate/apache/validators/ApacheIsbn10ValidatorTests.java
@@ -0,0 +1,14 @@
+package com.eplan.isbnfinder.validate.apache.validators;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ApacheIsbn10ValidatorTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
