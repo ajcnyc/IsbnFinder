cb81a5bfe3abdbb753c43eb353fff309228a8f2f
Alex Cohen <alexanderjosephcohen@gmail.com>
2023-02-23 11:17:11 -0500

    Completed headers for testing requests in RequestResponseFormatter

--- isbnfinder/src/test/java/com/eplan/isbnfinder/requestResponseFormatter/RequestResponseFormatterTests.java
+++ isbnfinder/src/test/java/com/eplan/isbnfinder/requestResponseFormatter/RequestResponseFormatterTests.java
@@ -2,13 +2,45 @@ package com.eplan.isbnfinder.requestResponseFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Request and Response Formatting Test Case")
 class RequestResponseFormatterTests {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@DisplayName("Test Formatting a Good CSV Request with Good ISBNs")
+	void testFormatGoodRequest() {
+		
 	}
 
+	@Test
+	@DisplayName("Test Formatting a Bad CSV Request with Good ISBNs")
+	void testFormatBadCsvRequest() {
+		
+	}
+	
+	@Test
+	@DisplayName("Test Formatting a Good CSV Request with Bad ISBNs")
+	void testFormatBadIsbnsRequest() {
+		
+	}
+	
+	@Test
+	@DisplayName("Test Formatting a Bad CSV Request with Bad ISBNs")
+	void testFormatBadCsvAndIsbnsRequest() {
+		
+	}
+	
+	@Test
+	@DisplayName("Test Formatting a Request That Contains Random Junk")
+	void testJunkRequest() {
+		
+	}
+	
+	@Test
+	@DisplayName("Test Formatting a Bad Request That's empty")
+	void testFormatEmptyRequest() {
+		
+	}
 }
