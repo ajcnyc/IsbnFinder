81b1a964a32ef496df3c254c4afb7cdff15b6707
Alex Cohen <alexanderjosephcohen@gmail.com>
2023-02-23 12:03:45 -0500

    Wrote just enough in formatRequest method to pass first test case

--- isbnfinder/src/main/java/com/eplan/isbnfinder/requestResponse/formatter/RequestResponseFormatter.java
+++ isbnfinder/src/main/java/com/eplan/isbnfinder/requestResponse/formatter/RequestResponseFormatter.java
@@ -1,5 +1,23 @@
 package com.eplan.isbnfinder.requestResponse.formatter;
 
+/**
+ * Formats requests from the frontend to the backend and responses from the
+ * backend to the frontend.
+ * 
+ * @author Alex Cohen
+ *
+ */
 public class RequestResponseFormatter {
+
+	/**
+	 * Formats a CSV request (Comma-Separated ISBNs) String from the frontend as a
+	 * String array
+	 * 
+	 * @param csvRequest The CSV request to format (Comma-Separated ISBNs)
+	 * @return A String array of ISBNs
+	 */
+	public String[] formatRequest(String csvRequest) {
+		return csvRequest.split(",");
+	}
 
 }
