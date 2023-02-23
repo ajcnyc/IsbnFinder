6cc6368f06e8507a54a40b46768b491da7a60ba8
Alex Cohen <alexanderjosephcohen@gmail.com>
2023-02-23 13:17:29 -0500

    Finished Isbn class. Only getters & setters.

--- isbnfinder/src/main/java/com/eplan/isbnfinder/validate/Isbn.java
+++ isbnfinder/src/main/java/com/eplan/isbnfinder/validate/Isbn.java
@@ -1,5 +1,62 @@
 package com.eplan.isbnfinder.validate;
 
+/**
+ * Represents an ISBN Number Whose Validity is Known
+ * 
+ * @author Alex Cohen
+ *
+ */
 public class Isbn {
+	
+	private String isbn_; // The ISBN Number
+	private boolean validity_; // Whether the ISBN is valid or not
+	
+	/**
+	 * Creates a new ISBN Number
+	 * 
+	 * @param isbn The ISBN number as a String
+	 * @param validity Whether the ISBN is valid or not
+	 */
+	public Isbn(String isbn, boolean validity) {
+		isbn_ = isbn;
+		validity_ = validity;
+	}
 
+	/**
+	 * Gets the ISBN number as a String
+	 * 
+	 * @return The ISBN number
+	 */
+	public String getIsbn() {
+		return isbn_;
+	}
+
+	/**
+	 * Sets the ISBN number
+	 * 
+	 * @param isbn The ISBN number to set
+	 */
+	public void setIsbn(String isbn) {
+		isbn_ = isbn;
+	}
+
+	/**
+	 * Checks if this ISBN is valid
+	 * 
+	 * @return true if valid, false if not
+	 */
+	public boolean isValid() {
+		return validity_;
+	}
+
+	/**
+	 * Sets whether this ISBN is valid
+	 * 
+	 * @param validity true if valid, false if not
+	 */
+	public void setValidity(boolean validity) {
+		validity_ = validity;
+	}
+	
+	
 }
