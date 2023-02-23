0c64e1b2ad0dff1526e20ce0e21e85ed3e226a68
Alex Cohen <alexanderjosephcohen@gmail.com>
2023-02-23 11:48:12 -0500

    Implemented 1st RequestResponseFormatter Test, and modified test headers

--- isbnfinder/src/test/java/com/eplan/isbnfinder/requestResponseFormatter/RequestResponseFormatterTests.java
+++ isbnfinder/src/test/java/com/eplan/isbnfinder/requestResponseFormatter/RequestResponseFormatterTests.java
@@ -2,45 +2,72 @@ package com.eplan.isbnfinder.requestResponseFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import com.eplan.isbnfinder.requestResponse.formatter.RequestResponseFormatter;
 
 @DisplayName("Request and Response Formatting Test Case")
 class RequestResponseFormatterTests {
 
+	private RequestResponseFormatter formatter_;
+	
+	@BeforeEach
+	void setUp() {
+		formatter_ = new RequestResponseFormatter();
+	}
+	
 	@Test
-	@DisplayName("Test Formatting a Good CSV Request with Good ISBNs")
+	@DisplayName("Test Formatting a Good CSV Request")
 	void testFormatGoodRequest() {
-		
+		// 200 is Okay, Because We're Only Testing the CSV Parsing Here, not ISBN Validity
+		String requestString = "0-06-097329-3,9781621291657,978-1-119-84766-3,200";
+		String[] isbnArr = formatter_.formatRequest(requestString);
+		assertEquals(isbnArr.length, 4);
+	}
+	
+	@Test
+	@DisplayName("Test Formatting a Good CSV Request, but with Extra Whitespace")
+	void testFormatWhitespaceRequest() {
+		fail("Not yet implemented");
 	}
 
 	@Test
-	@DisplayName("Test Formatting a Bad CSV Request with Good ISBNs")
-	void testFormatBadCsvRequest() {
-		
+	@DisplayName("Test Formatting a Bad CSV Request That's All Commas")
+	void testAllCommasRequest() {
+		fail("Not yet implemented");
 	}
 	
 	@Test
-	@DisplayName("Test Formatting a Good CSV Request with Bad ISBNs")
-	void testFormatBadIsbnsRequest() {
-		
+	@DisplayName("Test Formatting a Bad CSV Request That's All Commas Except 1 ISBN in the Middle")
+	void testAllCommasRequestButMiddle() {
+		fail("Not yet implemented");
 	}
 	
 	@Test
-	@DisplayName("Test Formatting a Bad CSV Request with Bad ISBNs")
-	void testFormatBadCsvAndIsbnsRequest() {
-		
+	@DisplayName("Test Formatting a Bad CSV Request That's All Commas Except 1 ISBN at the Start")
+	void testAllCommasRequestButStart() {
+		fail("Not yet implemented");
 	}
 	
 	@Test
-	@DisplayName("Test Formatting a Request That Contains Random Junk")
+	@DisplayName("Test Formatting a Bad CSV Request That's All Commas Except 1 ISBN at the End")
+	void testAllCommasRequestButEnd() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	@DisplayName("Test Formatting a Bad Request That Contains Random Junk")
 	void testJunkRequest() {
-		
+		fail("Not yet implemented");
 	}
 	
 	@Test
 	@DisplayName("Test Formatting a Bad Request That's empty")
 	void testFormatEmptyRequest() {
-		
+		fail("Not yet implemented");
 	}
+	
+	// TODO: TEST FORMATTING RESPONSES_________________________________________
 }
