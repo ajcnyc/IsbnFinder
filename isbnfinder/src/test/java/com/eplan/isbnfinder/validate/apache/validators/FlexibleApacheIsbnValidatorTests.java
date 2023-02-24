70d4e74b1a1759bc2a4cac9b0cdc93fbc9e9ec3d
Alex Cohen <alexanderjosephcohen@gmail.com>
2023-02-24 14:17:42 -0500

    Completed FlexibleApacheIsbnValidatorTests

--- isbnfinder/src/test/java/com/eplan/isbnfinder/validate/apache/validators/FlexibleApacheIsbnValidatorTests.java
+++ isbnfinder/src/test/java/com/eplan/isbnfinder/validate/apache/validators/FlexibleApacheIsbnValidatorTests.java
@@ -46,46 +46,31 @@ class FlexibleApacheIsbnValidatorTests {
 			assertEquals(validated[i].isValid(), true);
 		}
 	}
-	
-//	@Test
-//	@DisplayName("Implementation Test Validating 3 Good Strings")
-//	void implementationTestValidate3GoodStrings() {
-//		String[] isbnStrs = { "978-1-119-84766-3", "9781621291657", "978-0-470-47977-3" };
-//		Isbn[] validated = validator_.validate(isbnStrs);
-//
-//		assertEquals(validated.length, 3);
-//		assertEquals(validated[0].isbn, isbnStrs[0]);
-//		assertEquals(validated[0].isValid(), true);
-//		assertEquals(validated[1].isbn, isbnStrs[1]);
-//		assertEquals(validated[1].isValid(), true);
-//		assertEquals(validated[2].isbn, isbnStrs[2]);
-//		assertEquals(validated[2].isValid(), true);
-//	}
-//
-//	@Test
-//	@DisplayName("Implementation Test Validating Some Good and Bad Strings")
-//	void implementationTestValidateSomeGoodAndBad() {
-//		String[] isbnStrs = { "50", "0-06-097329-3", "9781621291657", "apples", "978-1-119-84766-3", "\n",
-//				"0-87975-705-1", "978-0-470-47977-3" };
-//		Isbn[] validated = validator_.validate(isbnStrs);
-//
-//		assertEquals(validated.length, 8);
-//		assertEquals(validated[0].isbn, isbnStrs[0]);
-//		assertEquals(validated[0].isValid(), false);
-//		assertEquals(validated[1].isbn, isbnStrs[1]);
-//		assertEquals(validated[1].isValid(), false);
-//		assertEquals(validated[2].isbn, isbnStrs[2]);
-//		assertEquals(validated[2].isValid(), true);
-//		assertEquals(validated[3].isbn, isbnStrs[3]);
-//		assertEquals(validated[3].isValid(), false);
-//		assertEquals(validated[4].isbn, isbnStrs[4]);
-//		assertEquals(validated[4].isValid(), true);
-//		assertEquals(validated[5].isbn, isbnStrs[5]);
-//		assertEquals(validated[5].isValid(), false);
-//		assertEquals(validated[6].isbn, isbnStrs[6]);
-//		assertEquals(validated[6].isValid(), false);
-//		assertEquals(validated[7].isbn, isbnStrs[7]);
-//		assertEquals(validated[7].isValid(), true);
-//	}
+
+	@Test
+	@DisplayName("Implementation Test Validating Some Good and Bad ISBN 10s and 13s")
+	void implementationTestValidateSomeGoodAndBad() {
+		String[] isbnStrs = { "50", "0-06-097329-3", "9781621291657", "apples", "978-1-119-84766-3", "\n",
+				"0-87975-705-1", "978-0-470-47977-3" };
+		Isbn[] validated = validator_.validate(isbnStrs);
+
+		assertEquals(validated.length, 8);
+		assertEquals(validated[0].isbn, isbnStrs[0]);
+		assertEquals(validated[0].isValid(), false);
+		assertEquals(validated[1].isbn, isbnStrs[1]);
+		assertEquals(validated[1].isValid(), true);
+		assertEquals(validated[2].isbn, isbnStrs[2]);
+		assertEquals(validated[2].isValid(), true);
+		assertEquals(validated[3].isbn, isbnStrs[3]);
+		assertEquals(validated[3].isValid(), false);
+		assertEquals(validated[4].isbn, isbnStrs[4]);
+		assertEquals(validated[4].isValid(), true);
+		assertEquals(validated[5].isbn, isbnStrs[5]);
+		assertEquals(validated[5].isValid(), false);
+		assertEquals(validated[6].isbn, isbnStrs[6]);
+		assertEquals(validated[6].isValid(), true);
+		assertEquals(validated[7].isbn, isbnStrs[7]);
+		assertEquals(validated[7].isValid(), true);
+	}
 
 }
