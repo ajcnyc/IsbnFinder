a4d78032d23f13535f01bef701c8e8edca2c48f5
Alex Cohen <alexanderjosephcohen@gmail.com>
2023-02-25 15:27:42 -0500

    Added basic home component HTML

--- isbnfinder/src/main/resources/frontend/angular-isbn-finder/src/app/app.component.ts
+++ isbnfinder/src/main/resources/frontend/angular-isbn-finder/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-isbn-finder';
+  public title:string = 'ISBN Validator';
 }
