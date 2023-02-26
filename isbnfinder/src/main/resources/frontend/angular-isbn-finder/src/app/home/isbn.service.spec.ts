bc88a026fbcdc87495f7c381ad8de62df753a63e
Alex Cohen <alexanderjosephcohen@gmail.com>
2023-02-26 14:36:48 -0500

    Wrote failing implementation test of isbn.service in accordance with TDD

--- isbnfinder/src/main/resources/frontend/angular-isbn-finder/src/app/home/isbn.service.spec.ts
+++ isbnfinder/src/main/resources/frontend/angular-isbn-finder/src/app/home/isbn.service.spec.ts
@@ -8,7 +8,7 @@ describe('IsbnService', () => {
         isbnService = new IsbnService();
     });
 
-    it('should return the correct array of Isbn objects when validateIsbns is called', () => {
+    it('should mock return the correct array of Isbn objects when validateIsbns is called', () => {
         let validated: Isbn[] = [
             new Isbn("0-06-097329-3", true),
             new Isbn("9781621291657", true),
@@ -23,6 +23,23 @@ describe('IsbnService', () => {
         let isbns: Isbn[] = isbnService.validateIsbns("0-06-097329-3,9781621291657,5,a,,hi,1-1-1");
 
         expect(validateSpy).toHaveBeenCalled();
+
+        for (let i: number = 0; i < validated.length; i++) {
+            expect(isbns[i]).toEqual(validated[i]);
+        }
+    });
+    
+    it('should return the correct array of Isbn objects when validateIsbns is called', () => {
+        let validated: Isbn[] = [
+            new Isbn("0-06-097329-3", true),
+            new Isbn("9781621291657", true),
+            new Isbn("5", false),
+            new Isbn("a", false),
+            new Isbn("", false),
+            new Isbn("hi", false),
+            new Isbn("1-1-1", false)];
+
+        let isbns: Isbn[] = isbnService.validateIsbns("0-06-097329-3,9781621291657,5,a,,hi,1-1-1");
 
         for (let i: number = 0; i < validated.length; i++) {
             expect(isbns[i]).toEqual(validated[i]);
