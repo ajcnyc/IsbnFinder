ffda71168f20ad66f344f38a39f65e3c7f699da8
Alex Cohen <alexanderjosephcohen@gmail.com>
2023-02-27 17:12:17 -0500

    Created blank ManualIsbn13ValidatorService with tests

---
+++ isbnfinder/src/main/java/com/eplan/isbnfinder/validate/manual/ManualIsbnValidatorService.java
@@ -0,0 +1,16 @@
+package com.eplan.isbnfinder.validate.manual;
+
+import org.springframework.stereotype.Service;
+
+import com.eplan.isbnfinder.validate.IsbnValidatorService;
+
+/**
+ * Validator interface for Validators that use manual validation
+ * 
+ * @author Alex Cohen
+ *
+ */
+@Service
+public interface ManualIsbnValidatorService extends IsbnValidatorService {
+
+}
---
+++ isbnfinder/src/main/java/com/eplan/isbnfinder/validate/manual/validators/ManualIsbn13ValidatorService.java
@@ -0,0 +1,14 @@
+package com.eplan.isbnfinder.validate.manual.validators;
+
+import com.eplan.isbnfinder.validate.Isbn;
+import com.eplan.isbnfinder.validate.manual.ManualIsbnValidatorService;
+
+public class ManualIsbn13ValidatorService implements ManualIsbnValidatorService {
+
+	@Override
+	public Isbn[] validate(String... isbns) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
