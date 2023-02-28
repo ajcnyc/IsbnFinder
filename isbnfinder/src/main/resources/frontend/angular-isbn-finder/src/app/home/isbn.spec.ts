e07cc5578ddfdc02d290cc364fe4d99e662ca32d
Alex Cohen <alexanderjosephcohen@gmail.com>
2023-02-28 12:25:57 -0500

    Created & completed isbn.spec.ts to get all code coverage over 80%

---
+++ isbnfinder/src/main/resources/frontend/angular-isbn-finder/src/app/home/isbn.spec.ts
@@ -0,0 +1,37 @@
+import { Isbn } from './isbn';
+
+describe('Isbn', () => {
+    let isbn: Isbn;
+    let isbn2: Isbn;
+
+    it('should construct', () => {
+        isbn = new Isbn("test",false);
+        expect(isbn.getIsbn()).toEqual("test");
+        expect(isbn.getValidity()).toEqual(false);
+    });
+
+    it('should get', () => {
+        isbn = new Isbn("test",false);
+        expect(isbn.getIsbn()).toEqual("test");
+    });
+
+    it('should set', () => {
+        isbn = new Isbn("test",false);
+        isbn.setIsbn("test2");
+        expect(isbn.getIsbn()).toEqual("test2");
+    });
+
+    it('should equal only when equals', () => {
+        isbn = new Isbn("test",false);
+
+        isbn2 = new Isbn("test2",false);
+        expect(isbn.equals(isbn2)).toEqual(false);
+        isbn2 = new Isbn("test",true);
+        expect(isbn.equals(isbn2)).toEqual(false);
+        isbn2 = new Isbn("test2",true);
+        expect(isbn.equals(isbn2)).toEqual(false);
+        isbn2 = new Isbn("test",false);
+        expect(isbn.equals(isbn2)).toEqual(true);
+
+    });
+});
