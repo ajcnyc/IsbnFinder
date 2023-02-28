e3e948e2e455987f036d313618fa9995d63d7cbe
Alex Cohen <alexanderjosephcohen@gmail.com>
2023-02-27 20:19:45 -0500

    Got Jasmine implementation test of IsbnService working!

--- isbnfinder/src/main/resources/frontend/angular-isbn-finder/src/app/home/isbn.service.spec.ts
+++ isbnfinder/src/main/resources/frontend/angular-isbn-finder/src/app/home/isbn.service.spec.ts
@@ -1,11 +1,13 @@
 import { Isbn } from './isbn';
 import { IsbnService } from './isbn.service';
 
-import { TestBed, ComponentFixture, fakeAsync, tick } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 describe('IsbnService', () => {
-    let httpMock: HttpTestingController;
+    //let httpClient: HttpClient;
+    let httpTestingController: HttpTestingController;
     let isbnService: IsbnService;
 
     beforeEach(() => {
@@ -14,38 +16,18 @@ describe('IsbnService', () => {
             providers: [IsbnService],
             declarations: []
         });
-        httpMock = TestBed.inject(HttpTestingController);
+
+        // httpClient = TestBed.inject(HttpClient);
+        httpTestingController = TestBed.inject(HttpTestingController);
         isbnService = TestBed.inject(IsbnService);
-        //jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
     });
 
     afterEach(() => {
-        httpMock.verify();
+        httpTestingController.verify();
     });
 
-    it('should mock return the correct array of Isbn objects when validateIsbns is called', () => {
-        let validated: Isbn[] = [
-            new Isbn("0-06-097329-3", true),
-            new Isbn("9781621291657", true),
-            new Isbn("5", false),
-            new Isbn("a", false),
-            new Isbn("", false),
-            new Isbn("hi", false),
-            new Isbn("1-1-1", false)];
-
-        const validateSpy = spyOn(isbnService, 'validateIsbns').and.returnValue(validated);
-
-        let isbns: Isbn[] = isbnService.validateIsbns("0-06-097329-3,9781621291657,5,a,,hi,1-1-1");
-
-        expect(validateSpy).toHaveBeenCalled();
-
-        for (let i: number = 0; i < validated.length; i++) {
-            expect(isbns[i]).toEqual(validated[i]);
-        }
-    });
-
-    // it('should return the correct array of Isbn objects when validateIsbns is called', () => {
-    //     const validated: Isbn[] = [
+    // it('should mock return the correct array of Isbn objects when validateIsbns is called', () => {
+    //     let validated: Isbn[] = [
     //         new Isbn("0-06-097329-3", true),
     //         new Isbn("9781621291657", true),
     //         new Isbn("5", false),
@@ -54,26 +36,37 @@ describe('IsbnService', () => {
     //         new Isbn("hi", false),
     //         new Isbn("1-1-1", false)];
 
-    //     let isbns: Isbn[] = [];
+    //     const validateSpy = spyOn(isbnService, 'validateIsbns').and.returnValue(validated);
 
-    //     async function f() {
-    //         isbns = isbnService.validateIsbns("0-06-097329-3,9781621291657,5,a,,hi,1-1-1").subscribe((result: Isbn[]) => {
-    //             isbns = result;
-    //             return Promise.resolve(1);
-    //         });
-    //     };
+    //     let isbns: Isbn[] = isbnService.validateIsbns("0-06-097329-3,9781621291657,5,a,,hi,1-1-1");
 
-    //     function f2() {
-    //         expect(isbns.length).toEqual(validated.length);
-    //         expect(Array.isArray(isbns)).toBeTruthy();
-    //         for (let i: number = 0; i < validated.length; i++) {
-    //             console.log(isbns[i].getIsbn());
-    //             expect(isbns[i].equals("a"));
-    //         };
+    //     expect(validateSpy).toHaveBeenCalled();
+
+    //     for (let i: number = 0; i < validated.length; i++) {
+    //         expect(isbns[i]).toEqual(validated[i]);
     //     }
-
-    //     f().then(f2);
-
-
     // });
+    it('should return the correct array of Isbn objects when validateIsbns is called', (done: DoneFn) => {
+        const validated: Isbn[] = [
+            new Isbn("0-06-097329-3", true),
+            new Isbn("9781621291657", true),
+            new Isbn("5", false),
+            new Isbn("a", false),
+            new Isbn("", false),
+            new Isbn("hi", false),
+            new Isbn("1-1-1", false)];
+
+        isbnService.validateIsbns("0-06-097329-3,9781621291657,5,a,,hi,1-1-1").subscribe((result: Isbn[]) => {
+            expect(result.length).toEqual(validated.length);
+            expect(Array.isArray(result)).toBeTruthy();
+            for (let i: number = 0; i < validated.length; i++) {
+                expect(result[i].equals(validated[i])).toBeTruthy();
+            }
+            done();
+        });
+        
+        const req = httpTestingController.expectOne('http://localhost:8080/api/validate');
+        expect(req.request.method).toEqual('POST');
+        req.flush(validated);
+    });
 });
